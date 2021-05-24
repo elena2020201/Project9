e46922b45bc8e3dae7527630d0db2509a9897543
elena2020201 <elenarus1992@mail.ru>
2021-05-24 19:56:44 +0300

    Добавьте файлы проекта.

---
+++ статич метод символин масс результат целочисл м с кодом/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace статич_метод_символин_масс_результат_целочисл_м_с_кодом
+{
+    class Program
+    {
+        static char[] rands(int n)
+        {
+            Random rnd = new Random();
+            char[] symbs = new char[n];
+            for (int k = 0; k < symbs.Length; k++)
+            {
+                symbs[k] = (char)('A' + rnd.Next(26));
+
+            }
+
+            return symbs;
+        }
+        static void Main()
+        {
+            char[] B;
+            B = rands(10);
+            Console.WriteLine("Случайные символы: ");
+            foreach (char s in B)
+            {
+                Console.Write("|{0}", s);
+            }
+            Console.WriteLine("|");
+            Console.ReadLine();
+        
+            }
+
+        }
+    }
+
+        
---
+++ статич метод символин масс результат целочисл м с кодом/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("статич метод символин масс результат целочисл м с кодом")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("статич метод символин масс результат целочисл м с кодом")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ce9700ed-5daa-45fc-a182-cba8b479b888")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
